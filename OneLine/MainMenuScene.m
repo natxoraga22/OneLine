092527e8479bb0bfa8228c12d4f45c11775ea607
Natxo Raga <natxoraga22@gmail.com>
2015-03-01 06:58:34 +0100

    Main menu with play button

--- OneLine/MainMenuScene.m
+++ OneLine/MainMenuScene.m
@@ -7,10 +7,13 @@
 //
 
 #import "MainMenuScene.h"
+#import "GameScene.h"
 
 
 @interface MainMenuScene()
 @property (strong, nonatomic) SKLabelNode *gameTitle;
+@property (strong, nonatomic) SKSpriteNode *playButton;
+@property (nonatomic) BOOL playPressed;
 @end
 
 
@@ -24,11 +27,12 @@
     
     // Scene elements
     [self addChild:self.gameTitle];
+    [self addChild:self.playButton];
 }
 
 static NSString *const GAME_TITLE_FONT_NAME = @"HelveticaNeue";
 static NSString *const GAME_TITLE = @"One Line";
-static const CGFloat GAME_TITLE_SIZE_SCALE_FACTOR = 1.0/6.0;
+static const CGFloat GAME_TITLE_SCALE_FACTOR = 1.0/6.0;
 static const CGFloat GAME_TITLE_VERTICAL_ALIGN_FACTOR = 2.0/3.0;
 
 - (SKLabelNode *)gameTitle
@@ -37,11 +41,51 @@ static const CGFloat GAME_TITLE_VERTICAL_ALIGN_FACTOR = 2.0/3.0;
         _gameTitle = [[SKLabelNode alloc] initWithFontNamed:GAME_TITLE_FONT_NAME];
         _gameTitle.text = GAME_TITLE;
         _gameTitle.fontColor = [SKColor whiteColor];
-        _gameTitle.fontSize = self.view.bounds.size.width*GAME_TITLE_SIZE_SCALE_FACTOR;
+        _gameTitle.fontSize = self.view.bounds.size.width*GAME_TITLE_SCALE_FACTOR;
         _gameTitle.position = CGPointMake(CGRectGetMidX(self.view.bounds),
                                          CGRectGetMinY(self.view.bounds) + CGRectGetHeight(self.view.bounds)*GAME_TITLE_VERTICAL_ALIGN_FACTOR);
     }
     return _gameTitle;
+}
+
+static NSString *const PLAY_BUTTON_IMAGE_NAME = @"Player.png";  //TODO: Change to PlayButton.png
+static NSString *const PLAY_BUTTON_NAME = @"PlayButton";
+static const CGFloat PLAY_BUTTON_SCALE_FACTOR = 1.0/2.0;
+
+- (SKSpriteNode *)playButton
+{
+    if (!_playButton) {
+        _playButton = [[SKSpriteNode alloc] initWithImageNamed:PLAY_BUTTON_IMAGE_NAME];
+        _playButton.name = PLAY_BUTTON_NAME;
+        _playButton.position = CGPointMake(CGRectGetMidX(self.view.bounds), CGRectGetMidY(self.view.bounds));
+        [_playButton setSize:CGSizeMake(CGRectGetWidth(self.view.bounds)*PLAY_BUTTON_SCALE_FACTOR,
+                                        CGRectGetWidth(self.view.bounds)*PLAY_BUTTON_SCALE_FACTOR)];
+    }
+    return _playButton;
+}
+
+static const CGFloat PLAY_BUTTON_HIGHLIGHTED_SCALE = 1.15;
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    CGPoint touchLocation = [[touches anyObject] locationInNode:self];
+    SKNode *node = [self nodeAtPoint:touchLocation];
+    
+    if ([node.name isEqualToString:PLAY_BUTTON_NAME]) {
+        [self.playButton setScale:PLAY_BUTTON_HIGHLIGHTED_SCALE];
+        self.playPressed = YES;
+    }
+}
+
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    if (self.playPressed) {
+        [self.playButton setScale:1.0];
+        
+        // Present GameScene
+        GameScene *gameScene = [[GameScene alloc] initWithSize:self.size];
+        [self.view presentScene:gameScene];
+    }
 }
 
 @end
